15c745ab3d56494fa1f962d25f97e47ef1929a9c
Tenorio09 <eneas.matias@escola.pr.gov.br>
2026-08-18 12:13:03 -0300

    .

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const caixaPrincipal = document.querySelector(".caixa-principal")
 const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativas")
 const caixaResultado = document.querySelector(".caixa-resultado")
-const TextoResultado = document.querySelector(".texto-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
 
 
 
