d5f62a7a35d805d2bd0adc28436d3ced55ff6db1
yuri2612w <wesley.yuri.santos@escola.pr.gov.br>
2026-08-20 09:10:57 -0300

    .

--- main.js
+++ main.js
@@ -80,7 +80,12 @@ let perguntaAtual;
 let historiaFinal = ""
 
 function mostraPergunta(){
-
+    if(atual >= perguntas.length) {
+        mostraResultado()
+        return    
+    }
+    perguntaAtual = perguntas[atual]
+    caixaPerguntas.textoContent = perguntaAtual
 }
-    
 
+mostraPergunta()
