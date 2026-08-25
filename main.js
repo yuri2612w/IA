8a2b53943560c9404cd2fa222729d5050c13abe9
yuri2612w <wesley.yuri.santos@escola.pr.gov.br>
2026-08-25 11:47:44 -0300

    .

--- main.js
+++ main.js
@@ -92,6 +92,7 @@ function mostraPergunta(){
 function mostraAlternativas(){
         for(const alternativa of perguntaAtual.alternativa){
             const botaoalternativas = document.createElement("button")
+            
         }
         
     }
