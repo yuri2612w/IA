3cbc520c799bcb859bc435bbc2016fb0ce79f7cd
yuri2612w <wesley.yuri.santos@escola.pr.gov.br>
2026-08-25 12:22:18 -0300

    .

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativas")
 const caixaResultado = document.querySelector(".caixa-resultado")
 const textoResultado = document.querySelector(".texto-resultado")
-
+ 
 
 
 
