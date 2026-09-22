a4702dd48ec742409d6bbdf407401ab4828c004e
Tenorio09 <eneas.matias@escola.pr.gov.br>
2026-09-22 11:58:24 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import {aleatorio} from "./aleatorio.js"
+import {pergunta} from "./pergunta.js"
 const caixaPrincipal = document.querySelector(".caixa-principal")
 const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativas")
